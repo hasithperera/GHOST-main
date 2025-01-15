d032890f687f4805cf9aec09593593ea332c0e88
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2025-01-14 19:35:41 -0500

    tested log file conting and sensor data ~1.0s

---
+++ src/config.py
@@ -0,0 +1,9 @@
+## Global configuration is saved here
+
+
+data_location = '/opt/data/ahe/'
+data_state = '/opt/data/config'
+
+
+telemetry_location = '/opt/data/telemetry'
+log_file = ""
--- src/main.py
+++ src/main.py
@@ -6,9 +6,12 @@ import time
 import sys
 import os
 
-
+import config as cfg
 
 from DFRobot_VEML7700 import *
+from ahe import *  #basic file io functions
+
+import numpy as np
 
 veml7700 = DFRobot_VEML7700_I2C(bus_num = 1)
 
@@ -25,11 +28,24 @@ if __name__=='__main__':
     
     print('[i] Start')
     err = lux_init()
+    file_num = get_bin_log_id()
+    print(f"[i] save loc:{cfg.data_location}")
+    print(f"[i] {file_num} bin files found")
 
+    cfg.log_file = f"{cfg.data_location}light_{file_num+1}.bin"
+    #exit()
+
+
+    t_s = time.time()
     #while(True):
-    for i in range(1,20):
-        t_o = time.time()
-        lux = veml7700.get_ALS_lux()
-        time.sleep(.1)
-        
-        print("{},{}".format(time.time()-t_o,lux))
+
+
+    data = np.zeros([100]);
+    for i in range(1,100):
+        #t_o = time.time()
+        data[i] = veml7700.get_ALS_lux()
+        time.sleep(.01)
+
+        #print("{},{}".format(time.time()-t_o,lux))
+    print(f"\n total time:{time.time()-t_s}")
+    print(data)
