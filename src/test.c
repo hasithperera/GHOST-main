63092e4202946f41e6aa618702eec78c54a27d88
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2025-02-16 00:35:09 -0500

    added omp for multi serial rx

--- src/test.c
+++ src/test.c
@@ -1,4 +1,4 @@
-#define TERMINAL    "/dev/ttyS1"
+#define TERMINAL    "/dev/ttyUSB0"
 
 #include <errno.h>
 #include <fcntl.h> 
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+
+
+#include <omp.h>
 
 int set_interface_attribs(int fd, int speed)
 {
@@ -59,26 +62,43 @@ void set_mincount(int fd, int mcount)
         printf("Error tcsetattr: %s\n", strerror(errno));
 }
 
+#define ports 2
 
 int main()
 {
-    char *portname = TERMINAL;
-    int fd;
-    int rdlen;
-    char buf[10];
+    char portname[20];
 
 
-    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) {
-        printf("Error opening %s: %s\n", portname, strerror(errno));
-        return -1;
-    }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
-    set_interface_attribs(fd, B115200);
-    //set_mincount(fd, 0);                /* set to pure timed read */
+    int fd[ports];
+    int rdlen[ports];
+    char buf[2][10];
 
-    tcflush(fd, TCIFLUSH);  //flush remove junk
-    rdlen = read(fd, buf, sizeof(buf) - 1);
-    printf("%s",buf);
-    printf("\n%d",rdlen);
+	for(int i=0;i<ports;i++){
+		sprintf(portname,"/dev/ttyUSB%d",i);
+		printf("%s\n",portname);
+    	fd[i] = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    	if (fd[i] < 0) {
+        	printf("Error opening %s: %s\n", portname, strerror(errno));
+        	return -1;
+    	}
+    	/*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    	set_interface_attribs(fd[i], B115200);
+    	//set_mincount(fd, 0);                /* set to pure timed read */
+    	tcflush(fd[i], TCIFLUSH);  //flush remove junk
+	}
+
+    
+    #pragma omp parallel num_threads(2)
+	{
+        int thread_id = omp_get_thread_num();
+		int tmp;
+	       
+
+		while(1){
+			tmp= read(fd[thread_id], buf[thread_id],10);	
+			if(tmp > 0){
+    			printf("%d-%s\n",thread_id,buf[thread_id]);
+			}
+		}
+	}
 }
