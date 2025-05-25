c68c69944c2460543594328b9e7291f7c0438f2c
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2025-05-25 10:36:20 -0400

    segmented 3040x4040 image tx

--- src/test_img.py
+++ src/test_img.py
@@ -29,8 +29,23 @@ print(np.shape(gray))
 #cv2.imshow('gray',image)
 #cv2.waitKey(0)
 
-h = 648;
-w = 648;
+h = 3040
+w = 4040
+
+w_box = 202 # box size in packet
+
+
+def im2hex_4bit(img,n_packets):
+    ''' build custom packet
+
+    img: data
+    i: row index
+    n_packets: sub packets per row
+    '''
+    width = np.size[1]
+    w_lenght = int(width/n_packets)
+
+    return 0
 
 
 
@@ -43,7 +58,29 @@ def img2base91(img):
 def img2hex(img,i):
 	hex_array = np.array([format(x, '02x') for x in img[i,:]])
 	row = "".join(hex_array)
+	#print("test")
 	return row
+
+
+
+# chatGPT unction to encode np data as binary
+import struct
+from io import BytesIO
+
+def send_array(sock, arr):
+    # Serialize array to bytes
+	#memfile = BytesIO()
+    # np.save writes shape, dtype, etc.
+	#np.save(memfile, arr)
+	#memfile.seek(0)
+	#data = memfile.read()
+	data = arr.tobytes()
+    # Prefix with message length
+	length = len(data)
+	#print(f"bin len:{length}")
+    #sock.sendto(struct.pack('!I', length))  # Network byte order
+	sock.sendto(data,(UDP_IP, UDP_PORT))
+
 
 # base91 encoding 
 data = img2base91(gray)
@@ -51,16 +88,42 @@ data2 = img2hex(gray,0)
 
 # create a socket to send the data via UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(data.encode('utf-8'),(UDP_IP, UDP_PORT))
-sock.sendto(data2.encode('utf-8'),(UDP_IP, UDP_PORT))
 
-
+#i = 3029
+#j = 18
+#w_length = int(4040/20)
+#print(w_length)
+#send_array(sock,gray[i,j*w_length:(j+1)*w_length])
+#print("done UDP sending")
+#exit()
 t_start = time.time()
+
+
+#send bin
 for i in range(0,h):
-	
+		
+	w_length = int(4040/20) 
+	data_packet = "imr{:4x}:".format(i)
+	sock.sendto(data_packet.encode('utf-8'),(UDP_IP, UDP_PORT)) #row-header
 	data2 = img2hex(gray,i)
-	data_packet = "img{:04}::{}".format(i,data2)
-	sock.sendto(data_packet.encode('utf-8'),(UDP_IP, UDP_PORT))
+	for j in range(0,20):
+		data_packet=":{:02}|".format(j)
+		sock.sendto(data_packet.encode('utf-8'),(UDP_IP, UDP_PORT))
+		send_array(sock,gray[i,j*w_length:(j+1)*w_length])
+
+
+## send as hex
+#for i in range(0,h):
+		
+#	w_length = int(4040/20)*2 ## extRA 2 ACCOUNTS FOR HEX
+#	data_packet = "imr{:4x}:".format(i)
+#	sock.sendto(data_packet.encode('utf-8'),(UDP_IP, UDP_PORT)) #row-header
+#	data2 = img2hex(gray,i)
+#	for j in range(0,20):
+#		data_packet=":{:02}|{}".format(j,data2[j*w_length:(j+1)*w_length])
+#		sock.sendto(data_packet.encode('utf-8'),(UDP_IP, UDP_PORT))
+
+
 t_end = time.time()
 
 
